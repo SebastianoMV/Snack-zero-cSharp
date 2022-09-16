321c8a33f52a4ac8bff3af3f761d070eb86ab1ea
SebastianoMV <sebastianovisco@gmail.com>
2022-09-16 14:24:38 +0200

    Snack 2

--- Program.cs
+++ Program.cs
@@ -1,6 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Esercizio Snack 0");
 
+//SNACK 1
+Console.WriteLine("");
+Console.WriteLine("Snack 1");
+
 int firstNumber = 0;
 int secondNumber = 0;
 
@@ -21,4 +25,33 @@ else if (secondNumber > firstNumber)
 else
 {
     Console.WriteLine("I due numeri sono uguali");
+}
+
+
+
+//SNACK 2
+
+Console.WriteLine("");
+Console.WriteLine("Snack 2");
+
+string firstWord;
+string secondWord;
+
+Console.WriteLine("Inserisci la prima parola");
+firstWord = Console.ReadLine();
+
+Console.WriteLine("Inserisci la seconda parola");
+secondWord = Console.ReadLine();
+
+if (firstWord.Length > secondWord.Length)
+{
+    Console.WriteLine(firstWord + " è più lunga");
+}
+else if (secondWord.Length > firstWord.Length)
+{
+    Console.WriteLine(secondWord + " é più lunga");
+}
+else
+{
+    Console.WriteLine("Le due parole sono lunghe uguali");
 }
