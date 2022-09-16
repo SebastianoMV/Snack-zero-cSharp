80d52dbd1322c489ab4114ec2bc20c006bc66291
SebastianoMV <sebastianovisco@gmail.com>
2022-09-16 17:32:50 +0200

    Piccola miglioria Snack 7

--- Program.cs
+++ Program.cs
@@ -5,59 +5,59 @@ Console.WriteLine("Esercizio Snack 0");
 
 //SNACK 1
 
-//Console.WriteLine("");
-//Console.WriteLine("Snack 1");
+Console.WriteLine("");
+Console.WriteLine("Snack 1");
 
-//int firstNumber = 0;
-//int secondNumber = 0;
+int firstNumber = 0;
+int secondNumber = 0;
 
-//Console.WriteLine("Inserisci il primo numero");
-//firstNumber = Int32.Parse(Console.ReadLine());
+Console.WriteLine("Inserisci il primo numero");
+firstNumber = Int32.Parse(Console.ReadLine());
 
-//Console.WriteLine("Inserisci il secondo numero");
-//secondNumber = Int32.Parse(Console.ReadLine());
+Console.WriteLine("Inserisci il secondo numero");
+secondNumber = Int32.Parse(Console.ReadLine());
 
-//if (firstNumber > secondNumber)
-//{
-//    Console.WriteLine(firstNumber + " è più grande");
-//}
-//else if (secondNumber > firstNumber)
-//{
-//    Console.WriteLine(secondNumber + " é più grande");
-//}
-//else
-//{
-//    Console.WriteLine("I due numeri sono uguali");
-//}
+if (firstNumber > secondNumber)
+{
+    Console.WriteLine(firstNumber + " è più grande");
+}
+else if (secondNumber > firstNumber)
+{
+    Console.WriteLine(secondNumber + " é più grande");
+}
+else
+{
+    Console.WriteLine("I due numeri sono uguali");
+}
 
 
 
 //SNACK 2
 
-//Console.WriteLine("");
-//Console.WriteLine("Snack 2");
+Console.WriteLine("");
+Console.WriteLine("Snack 2");
 
-//string firstWord;
-//string secondWord;
+string firstWord;
+string secondWord;
 
-//Console.WriteLine("Inserisci la prima parola");
-//firstWord = Console.ReadLine();
+Console.WriteLine("Inserisci la prima parola");
+firstWord = Console.ReadLine();
 
-//Console.WriteLine("Inserisci la seconda parola");
-//secondWord = Console.ReadLine();
+Console.WriteLine("Inserisci la seconda parola");
+secondWord = Console.ReadLine();
 
-//if (firstWord.Length > secondWord.Length)
-//{
-//    Console.WriteLine(firstWord + " è più lunga");
-//}
-//else if (secondWord.Length > firstWord.Length)
-//{
-//    Console.WriteLine(secondWord + " é più lunga");
-//}
-//else
-//{
-//    Console.WriteLine("Le due parole sono lunghe uguali");
-//}
+if (firstWord.Length > secondWord.Length)
+{
+    Console.WriteLine(firstWord + " è più lunga");
+}
+else if (secondWord.Length > firstWord.Length)
+{
+    Console.WriteLine(secondWord + " é più lunga");
+}
+else
+{
+    Console.WriteLine("Le due parole sono lunghe uguali");
+}
 
 
 //SNACK 3
@@ -158,6 +158,7 @@ Console.WriteLine("");
 Console.WriteLine("Snack 7");
 
 int[] numbersSnack7;
+int arrayLength = 0;
 numbersSnack7 = new int[6];
 
 Console.WriteLine("Inserisci 6 numeri");
@@ -171,7 +172,8 @@ for (int i = 0; i < numbersSnack7.Length; i++)
     n = Int32.Parse(Console.ReadLine());
     if (n % 2 != 0)
     {
-        numbersSnack7[i] = n;
+        numbersSnack7[arrayLength] = n;
+        arrayLength++;
     }
 
     Console.WriteLine(numbersSnack7[i]);
