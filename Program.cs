83a0c8e9a16d84d5b8a96471578df7d49dcc1098
SebastianoMV <sebastianovisco@gmail.com>
2022-09-16 15:24:44 +0200

    Snack 7 Prima versione

--- Program.cs
+++ Program.cs
@@ -3,137 +3,169 @@ Console.WriteLine("Esercizio Snack 0");
 
 //SNACK 1
 
-Console.WriteLine("");
-Console.WriteLine("Snack 1");
+//Console.WriteLine("");
+//Console.WriteLine("Snack 1");
 
-int firstNumber = 0;
-int secondNumber = 0;
+//int firstNumber = 0;
+//int secondNumber = 0;
 
-Console.WriteLine("Inserisci il primo numero");
-firstNumber = Int32.Parse(Console.ReadLine());
+//Console.WriteLine("Inserisci il primo numero");
+//firstNumber = Int32.Parse(Console.ReadLine());
 
-Console.WriteLine("Inserisci il secondo numero");
-secondNumber = Int32.Parse(Console.ReadLine());
+//Console.WriteLine("Inserisci il secondo numero");
+//secondNumber = Int32.Parse(Console.ReadLine());
 
-if (firstNumber > secondNumber)
-{
-    Console.WriteLine(firstNumber + " è più grande");
-}
-else if (secondNumber > firstNumber)
-{
-    Console.WriteLine(secondNumber + " é più grande");
-}
-else
-{
-    Console.WriteLine("I due numeri sono uguali");
-}
+//if (firstNumber > secondNumber)
+//{
+//    Console.WriteLine(firstNumber + " è più grande");
+//}
+//else if (secondNumber > firstNumber)
+//{
+//    Console.WriteLine(secondNumber + " é più grande");
+//}
+//else
+//{
+//    Console.WriteLine("I due numeri sono uguali");
+//}
 
 
 
 //SNACK 2
 
-Console.WriteLine("");
-Console.WriteLine("Snack 2");
+//Console.WriteLine("");
+//Console.WriteLine("Snack 2");
 
-string firstWord;
-string secondWord;
+//string firstWord;
+//string secondWord;
 
-Console.WriteLine("Inserisci la prima parola");
-firstWord = Console.ReadLine();
+//Console.WriteLine("Inserisci la prima parola");
+//firstWord = Console.ReadLine();
 
-Console.WriteLine("Inserisci la seconda parola");
-secondWord = Console.ReadLine();
+//Console.WriteLine("Inserisci la seconda parola");
+//secondWord = Console.ReadLine();
 
-if (firstWord.Length > secondWord.Length)
-{
-    Console.WriteLine(firstWord + " è più lunga");
-}
-else if (secondWord.Length > firstWord.Length)
-{
-    Console.WriteLine(secondWord + " é più lunga");
-}
-else
-{
-    Console.WriteLine("Le due parole sono lunghe uguali");
-}
+//if (firstWord.Length > secondWord.Length)
+//{
+//    Console.WriteLine(firstWord + " è più lunga");
+//}
+//else if (secondWord.Length > firstWord.Length)
+//{
+//    Console.WriteLine(secondWord + " é più lunga");
+//}
+//else
+//{
+//    Console.WriteLine("Le due parole sono lunghe uguali");
+//}
 
 
 //SNACK 3
 
-Console.WriteLine("");
-Console.WriteLine("Snack 3");
+//Console.WriteLine("");
+//Console.WriteLine("Snack 3");
 
-int sum = 0;
+//int sum = 0;
 
-Console.WriteLine("Inserisci 10 numeri");
+//Console.WriteLine("Inserisci 10 numeri");
 
-for (int i = 0; i < 10; i++)
-{
-    Console.WriteLine("Inserisci il " + (i + 1) + "° numero");
-    int n = Int32.Parse(Console.ReadLine());
-    sum += n;
-}
+//for (int i = 0; i < 10; i++)
+//{
+//    Console.WriteLine("Inserisci il " + (i + 1) + "° numero");
+//    int n = Int32.Parse(Console.ReadLine());
+//    sum += n;
+//}
 
-Console.WriteLine("La somma è " + sum);
+//Console.WriteLine("La somma è " + sum);
 
 
 //SNACK 4
 
-Console.WriteLine("");
-Console.WriteLine("Snack 4");
+//Console.WriteLine("");
+//Console.WriteLine("Snack 4");
 
-int sumSnack4 = 0;
+//int sumSnack4 = 0;
 
-for (int i = 2; i <= 10; i++)
-{
-    sumSnack4 += i;
-}
+//for (int i = 2; i <= 10; i++)
+//{
+//    sumSnack4 += i;
+//}
 
-Console.WriteLine("La somma dei numeri da 2 a 10 è " + sumSnack4);
+//Console.WriteLine("La somma dei numeri da 2 a 10 è " + sumSnack4);
 
 
 //SNACK 5
 
-int numberSnack5 = 0;
+//Console.WriteLine("");
+//Console.WriteLine("Snack 5");
 
-Console.WriteLine("Inserisci un numero");
+//int numberSnack5 = 0;
 
-numberSnack5 = Int32.Parse(Console.ReadLine());
+//Console.WriteLine("Inserisci un numero");
 
-if (numberSnack5 % 2 == 0)
-{
-    Console.WriteLine("Il numero " + numberSnack5 + " è pari");
-}
-else
-{
-    Console.WriteLine("Il numero " + numberSnack5 + " non è pari, il numero " + (numberSnack5 + 1) + " è pari.");
-}
+//numberSnack5 = Int32.Parse(Console.ReadLine());
+
+//if (numberSnack5 % 2 == 0)
+//{
+//    Console.WriteLine("Il numero " + numberSnack5 + " è pari");
+//}
+//else
+//{
+//    Console.WriteLine("Il numero " + numberSnack5 + " non è pari, il numero " + (numberSnack5 + 1) + " è pari.");
+//}
 
 
 //SNACK 6
 
-string[] partyGatsby = { "Myrtle Wilson", "Daisy Buchanan", "Tom Buchanan", "Meyer Wolfsheim", "Jordan Baker", "Jay Gatsby" };
+//Console.WriteLine("");
+//Console.WriteLine("Snack 6");
 
-bool flagInvite = false;
+//string[] partyGatsby = { "Myrtle Wilson", "Daisy Buchanan", "Tom Buchanan", "Meyer Wolfsheim", "Jordan Baker", "Jay Gatsby" };
 
-string personSnack6 = "";
+//bool flagInvite = false;
 
-Console.WriteLine("Inserisci nome e cognome");
-personSnack6 = Console.ReadLine();
+//string personSnack6 = "";
 
-for (int i = 0; i < partyGatsby.Length; i++)
+//Console.WriteLine("Inserisci nome e cognome");
+//personSnack6 = Console.ReadLine();
+
+//for (int i = 0; i < partyGatsby.Length; i++)
+//{
+//    if(partyGatsby[i] == personSnack6)
+//    {
+//        flagInvite = true;
+//    }
+//}
+
+//if (flagInvite)
+//{
+//    Console.WriteLine("Benvenuto/a " + personSnack6 + ", lei è in lista, può partecipare al party");
+//}
+//else
+//{
+//    Console.WriteLine(personSnack6 + " lei non è invitato/a alla festa");
+//}
+
+
+//SNACK 7
+
+Console.WriteLine("");
+Console.WriteLine("Snack 7");
+
+int[] numbersSnack7 = {0 , 0, 0, 0, 0, 0 };
+
+Console.WriteLine("Inserisci 6 numeri");
+
+for (int i = 0; i < numbersSnack7.Length; i++)
 {
-    if(partyGatsby[i] == personSnack6)
+    int n = 0;
+
+    
+    Console.WriteLine("Inserisci il " + (i + 1) + "° numero");
+    n = Int32.Parse(Console.ReadLine());
+    if (n % 2 != 0)
     {
-        flagInvite = true;
+        numbersSnack7[i] = n;
     }
+
+    Console.WriteLine(numbersSnack7[i]);
 }
 
-if (flagInvite)
-{
-    Console.WriteLine("Benvenuto/a " + personSnack6 + ", lei è in lista, può partecipare al party");
-}
-else
-{
-    Console.WriteLine(personSnack6 + " lei non è invitato/a alla festa");
-}
